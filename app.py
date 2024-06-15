2064807ae428f1f55271a030058e1e256322f236
user <jiangzc210>
2024-06-14 20:56:03 -0400

    implement file removal implement redirect to index.html after action

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
-from flask import render_template
-from flask import request
+from flask import render_template, redirect, request, url_for
 from flask import send_from_directory
 from werkzeug.utils import secure_filename
 import os
@@ -10,7 +9,7 @@ app = Flask(__name__)
 @app.get("/")
 @app.get("/index")
 def index():
-    return render_template("index.html", filelist=[])
+    return render_template("index.html", filelist=os.listdir("./uploads"))
 
 
 @app.route('/upload', methods=['GET', 'POST'])
@@ -18,13 +17,19 @@ def upload():
     if request.method == 'POST':
         f= request.files['file']
         f.save(f"./uploads/{secure_filename(f.filename)}")
-    return index()
-
-@app.get('/list')
-def show():
-    return render_template("index.html", filelist=os.listdir("./uploads"))
+    return redirect(url_for('index'))
 
 @app.get("/uploads/<path:name>")
+def getUpload(name):
+    return send_from_directory("./uploads", secure_filename(name))
+
+@app.get("/<path:name>")
 def getfile(name):
-    return send_from_directory("./uploads", name)
+    return send_from_directory("./templates", name)
+
+@app.get("/remove/<path:name>")
+def deletefile(name):
+    if name in os.listdir("./uploads"):
+        os.remove(f"./uploads/{secure_filename(name)}")
+    return redirect(url_for('index'))
 
